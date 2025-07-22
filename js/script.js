0af105308288c8956bd8fd0bbdb460cd58236f25
KayTracer <cainaluz@gmail.com>
2025-07-21 21:39:04 -0300

    Terminei o cabeçalho. Adicionei o script.js para fazer as mudanças de estado do cabeçalho

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const elemHeader = document.querySelector("header");
+const elemMenu = document.querySelector(".js-menu");
+
+elemMenu.addEventListener("click", () => {
+  const barras = elemMenu.querySelectorAll("div");
+  const barraCima = barras[0];
+  const barraBaixo = barras[1];
+
+  elemHeader.classList.toggle("grid-rows-[3.75rem_1fr]");
+  elemHeader.classList.toggle("h-screen");
+
+  barraCima.classList.toggle("rotate-45");
+  barraCima.classList.toggle("translate-y-[0.28125rem]");
+
+  barraBaixo.classList.toggle("-rotate-45");
+  barraBaixo.classList.toggle("-translate-y-[0.28125rem]");
+});
