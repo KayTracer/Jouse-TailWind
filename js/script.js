52c7230d12c053fc9c33b6d6bb27b7adcefb6269
KayTracer <cainaluz@gmail.com>
2025-07-22 19:59:55 -0300

    Terminei uma parte da section, infelizmente a imagem não existe mais. Fiz umas funções com forEach no script

--- js/script.js
+++ js/script.js
@@ -15,3 +15,30 @@ elemMenu.addEventListener("click", () => {
   barraBaixo.classList.toggle("-rotate-45");
   barraBaixo.classList.toggle("-translate-y-[0.28125rem]");
 });
+
+//Carteira
+const elemImgCarteira = document.querySelector(".js-img-carteira");
+const elemTextoPersonalizacao = document.querySelector(".js-texto-cor");
+const elemsBolinha = document.querySelectorAll(".js-bolinhas");
+
+elemsBolinha.forEach((elemBolinha) => {
+  elemBolinha.addEventListener("click", () => {
+    const corAtual = elemImgCarteira.getAttribute("data-cor-atual");
+    const novaCor = elemBolinha.getAttribute("data-texto-cor");
+
+    elemsBolinha.forEach((elem) => {
+      elem.classList.remove("border-black");
+      elem.classList.add("border-gray-300");
+    });
+
+    elemBolinha.classList.add("border-black");
+    elemBolinha.classList.remove("border-gray-300");
+
+    elemTextoPersonalizacao.innerText =
+      elemBolinha.getAttribute("data-texto-cor");
+
+    elemImgCarteira.src = elemImgCarteira.src.replace(corAtual, novaCor);
+
+    elemImgCarteira.setAttribute("data-cor-atual", novaCor);
+  });
+});
